e9a206c2ed60cb1f7be0a28902c7fcdbdecfec5e
gittyvarshney <pv66645@gmail.com>
2020-07-24 23:10:48 +0530

    LRU Cache Implementation

--- lru_cache.cpp
+++ lru_cache.cpp
@@ -1,62 +1,83 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define MAX 100005
+//You have given a some limited memory capacity in which you have
+//to store key, value pairs you have to insert data & delete them according
+//to least recently used concept
+//It is implemented using the list stl having pair<int,int> value
+//and unordered_map having int as key and list iterator as value to store the
+//pointers of the memory locations of the linkedList
 
 class LRUCache
 {
-  list<int>dll;
-  unordered_map<int,int> m;
+  list<pair<int,int>>link_list; //to store key and value
+  unordered_map<int,list<pair<int,int>>::iterator> m; //key and value as iterator of link_list
+  //done in order to store the memory locations of the pointer
   int max_cap;
-  int curr_cap=0;
+  int curr_cap=0; //current capacity is 0 initially
 public:
 
   LRUCache(int capacity)
   {
-    max_cap = capacity;
+    max_cap = capacity; //max capacity is alloted by constructor
   }
 
   int get(int key)
   {
     if(m.find(key) == m.end())
     {
-      return -1;
+      return -1; //if map[key] pointer not found return -1
     }
     else
     {
-      dll.erase(key);
-      dll.push_front(key);
-      return m[key];
+      int value = m[key]->second; //value is second element of linkedlist pair
+      link_list.erase(m[key]);//erase this m[key] pointer to push at front
+      link_list.push_front(make_pair(key,value)); //make new same pointer
+      m[key] = link_list.begin(); // and rechange the address
+      return value;
     }
   }
 
   void put(int key, int value)
   {
-    if(max_cap == curr_cap)
+    if(max_cap == curr_cap)//if reached maximum capacity
     {
-      if(m.find(key) == m.end())
+      if(m.find(key) == m.end()) //if not found then discard least used
       {
-        m.erase(dll.back());
-        dll.pop_back();
+        m.erase(link_list.back().first);
+        link_list.pop_back();
       }
-      else
+      else //if found simply move it to the front
       {
-        dll.erase(key);
+        link_list.erase(m[key]);
       }
-      dll.push_front(key);
-      m[key] = value;
+      link_list.push_front(make_pair(key,value));
+      m[key] = link_list.begin();
     }
-    else
+    else //if not reached maximum capacity
     {
       if(m.find(key) == m.end())
       {
-        cur_cap++;
+        curr_cap++;
       }
       else
       {
-        dll.erase(key);
+        link_list.erase(m[key]);
       }
-      dll.push_front(key);
-      m[key] = value;
+      link_list.push_front(make_pair(key,value));
+      m[key] = link_list.begin();
     }
   }
 };
+
+int main()
+{
+  LRUCache* obj = new LRUCache(1);
+  obj->put(2,1);
+  cout << obj->get(2) << "\n";
+  obj->put(3,2);
+  cout << obj->get(2) << "\n";
+  cout << obj->get(3) << "\n";
+
+
+}
